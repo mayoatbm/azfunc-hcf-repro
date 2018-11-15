e16ffffb894ee236a9e66dcbe9d77766ab31b884
Mayo Atte <Olumayowa.Atte@BNSF.com>
2018-11-15 10:53:49 -0600

    Initial commit

---
+++ azfunc-hcf-repro/Startup.cs
@@ -0,0 +1,29 @@
+﻿using azfunc_hcf_repro;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+
+namespace azfunc_hcf_repro
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.AddHttpClient();
+
+            var serviceProvider = builder.Services.BuildServiceProvider();
+
+            var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+        }
+    }
+}
